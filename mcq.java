e0491a6d7c3c3e6cf44e29b2872c3c172c244f8b
Ajay Chaudhari <ajaychaudhari0559@gmail.com>
2026-06-09 15:16:37 +0530

    initial msg

---
+++ mcq.java
@@ -0,0 +1,288 @@
+// msq
+
+import java.util.*;
+public class mcq
+{
+	public static void main(String args[])
+	{
+	  Scanner sc=new Scanner(System.in);
+	  
+	  /* int a=10,b=5;
+	  System.out.println(a+b*2);*/
+	  
+	  /*int a=10;
+	  a+=5*2;
+	  System.out.println(a);*/
+	  
+	  /*int x=7;
+	  System.out.println(x > 5 && x < 10);*/
+	  
+	  /*int a=5;
+	  System.out.println((a*2)==10);*/
+	  
+	  /*int x=10;
+	  x-=3+2;
+	  System.out.println(x);*/
+	  
+	  /*int a=4,b=2;
+	  System.out.println((a>b)||(b>5));*/
+	  
+	  /*int a=3;
+	  a*=2+1;
+	  System.out.println(a);*/
+	  
+	  /*int a=10;
+	  boolean b=(a%2==0);
+	  System.out.println(b);*/
+	  
+	 /* int x=6;
+	  System.out.println(x++==6 && ++x==8);*/
+	  
+	  /*int a=8;
+	  System.out.println(a/4==2 || a*2==10);*/
+	  
+	  /*int x=5;
+	  x=x++ + ++x;
+	  System.out.println(x);*/
+	  
+	 /* int a=3,b=7;
+	  System.out.println(a==b || b>a);*/
+	  
+	  /*int a=6;
+	  boolean result=!(a<5);
+	  System.out.println(result);*/
+	  
+	  /*int x=9;
+	  System.out.println((x%2==0) ? "Even" :"odd") ;*/
+	  
+	  /*int a=5,b=3;
+	  System.out.println((a>b)&& (a+b>10));*/
+	  
+	  /*int a=6;
+	  System.out.println((a&3)+2);*/
+	  
+	 /* int a=4;
+	  System.out.println((a<<1)+(a>>1));*/
+	  
+	  /*int a=5,b=2;
+	  System.out.println((a|b)-(a&b));*/
+	  
+	  /*int a=3;
+	  System.out.println((a^1)*2);*/
+	  
+	  /*int a=7;
+	  System.out.println((a&1)==1 ? a*2 :a/2);*/
+	  
+	  /*int x=10;
+	  System.out.println((x<<1)-(x>>1));*/
+	  
+	  /*int x = 5;
+      System.out.println((~x + 1) == -x ? "Match" : "No Match");*/
+	  
+	 /*int x = 5;
+     System.out.println(++x + x++);*/
+	 
+	 /*int a = 2, b = 3;
+     System.out.println(a * b / a);*/
+	 
+	 /*int x = 5;
+     x *= 2 + 3;
+     System.out.println(x);*/
+	 
+	 /*int a = 2;
+     System.out.println(a + a++ + ++a);*/
+	 
+	 /*int a = 8;
+     System.out.println(a >>> 1);*/
+	 
+	 /*int a = 10, b = 20;
+     System.out.println(!(a > b));*/
+
+     /*int a = 5;
+     int b = a++ + ++a + --a + a--;
+     System.out.println(b);*/
+	 
+	 /*int x = 10;
+     System.out.println(x++ + x-- + ++x);*/
+
+     /*int a = 2, b = 3, c = 4;
+     System.out.println(a + b * c - a / b);*/
+	 
+	 /*int a = 5;
+     System.out.println(a++ * ++a);*/
+	 
+	 /*int x = 4;
+     int y = x++ + ++x * x--;
+     System.out.println(y);*/
+	 
+	 /*int a = 5;
+     a += a++ + ++a + --a;
+     System.out.println(a);*/
+
+	/*int x = 10;
+    System.out.println(x > 5 ? x < 15 ? 1 : 2 : 3);*/
+	
+    /*int a = 6, b = 4, c = 2;
+    System.out.println(a >> b - c);*/
+	
+	/*int a = 1;
+    System.out.println(a+++a+++a++);*/
+	
+	
+    /*int a = 10;
+    System.out.println(a++ + ++a * --a);*/
+	
+	/*int x = 5, y = 2;
+    System.out.println(x << y >> 1);*/
+
+    /*int a = 7;
+    System.out.println(~a + a++);*/
+
+    
+    /*int a = 10;
+    System.out.println((a++ > 10) && (++a > 12));*/
+
+    
+     /*int x = 2, y = 3;
+     System.out.println((x & y) | (x ^ y));*/
+
+    /*int a = 5;
+    System.out.println(a++ + ++a * a-- - --a);*/
+
+  /* int x = 7;
+   System.out.println(x >>> 2 << 1);*/
+   
+   /*int x = 5;
+   int y = x++ + ++x + x-- + --x;
+   System.out.println(y);*/
+
+   /*int x = 4;
+   x += x *= x -= 2;
+   System.out.println(x);*/
+
+   //int a = 2;
+   //System.out.println(a+++ ++a + --a);
+
+   //int a = 5;
+   //System.out.println((a++ > 4) ? (a++ < 7 ? 1 : 2) : 3);
+
+   //int a = 1;
+   //System.out.println((++a + a++) * (--a + a--));
+
+   /*int x = 3;
+   x = x-- + x++ + --x;
+   System.out.println(x);*/
+
+   /*int a = 2, b = 3, c = 1;
+   System.out.println(a++ > b-- && ++c > 1);*/
+
+   /*int a = 1, b = 2;
+   System.out.println(a++ + b++ + a++ * b++);*/
+
+   //int a = 4;
+   //System.out.println((a &= 3) + (a |= 2) + (a ^= 1));
+
+   //int a = 5, b = 10;
+   //System.out.println(a++ + b- - - ++a);
+
+/*int a = 10;
+int b = a++ + a++ + a++;
+System.out.println(b);*/
+
+/*int x = 4;
+x += x *= x -= 2;
+System.out.println(x);*/
+
+/*int n = 1234, sum = 0;
+while(n > 0){
+    sum += n % 10;
+    n /= 10;
+}
+System.out.println(sum);
+*/
+
+/*int n = 5, fact = 1;
+for(int i=1;i<=n;i++){
+    fact *= i;
+}
+System.out.println(fact);*/
+
+/*int n = 6, count = 0;
+for(int i=1;i<=n;i++){
+    if(n % i == 0) count++;
+}
+System.out.println(count);
+*/
+
+/*int n = 12345, rev = 0;
+while(n > 0){
+    rev = rev*10 + n%10;
+    n /= 10;
+}
+System.out.println(rev);*/
+
+/*int n = 10, sum = 0;
+for(int i=1;i<=n;i++){
+    if(i%2==0) sum += i;
+}
+System.out.println(sum);*/
+
+/*int n = 7, sum = 0;
+for(int i=1;i<=n;i+=2){
+    sum += i*i;
+}
+System.out.println(sum);*/
+
+/*int n = 7, sum = 0;
+for(int i=1;i<=n;i+=2){
+    sum += i*i;
+}
+System.out.println(sum);
+*/
+
+/*int n = 20;
+for(int i=2;i<=n;i+=2){
+    if(i%4==0) System.out.print(i+" ");
+}
+*/
+
+/*int n = 50, sum = 0;
+for(int i=1;i<=n;i++){
+    if(i%3==0 && i%5==0) sum += i;
+}
+System.out.println(sum);*/
+
+/*int code = 3;
+switch(code) {
+   case 1:
+   case 2: System.out.println("Low"); break;
+   case 3:
+   case 4: System.out.println("Medium"); break;
+   default: System.out.println("High");
+}*/
+
+int x = 3;
+switch(x) {
+    default: System.out.println("Default");
+    case 1: System.out.println("One");
+    case 2: System.out.println("Three");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	  
+	}
+}
